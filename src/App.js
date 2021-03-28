3cb32d1a0dac2376fc33ed906e0b73325ba86170
Tommy Willen <tommyallen1215@gmail.com>
2021-03-28 09:57:55 -0700

    added initial components that are blank and created routes. now I am going to sleep because my vaccine is kicking my butt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import MyNavbar from "./components/MyNavbar/MyNavbar";
+import ProductList from "./components/ProductList/ProductList";
+import Details from "./components/Details/Details";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default/Default";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MyNavbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyNavbar = () => {
+
+    return (
+        <div>
+           <h3> Hello from MyNavBar</h3> 
+        </div>
+    )
+}
+
+export default MyNavbar
